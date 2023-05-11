b94dddcbe9ffbd0b1fb35bfb42aad4201466f09e
Livia <livia.namba@gmail.com>
2023-05-10 22:13:30 -0300

    commit

--- script2.js
+++ script2.js
@@ -1,16 +1,5 @@
 
-document.getElementById('olho').addEventListener('mousedown', function() {
-    document.getElementById('pass').type = 'text';
-  });
-  
-  document.getElementById('olho').addEventListener('mouseup', function() {
-    document.getElementById('pass').type = 'password';
-  });
-  
-  document.getElementById('olho').addEventListener('mousemove', function() {
-    document.getElementById('pass').type = 'password';
-  });
-
-  function submitForm(){
-    alert('Registrado com sucesso! Aproveite e comece a comprar com a gente!!');
-  }
+document.getElementById("formulario").addEventListener("submit", function(event){
+  event.preventDefault(); 
+  alert("Cadastro realizado com sucesso!");
+});
